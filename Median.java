f87b3f2b713e2da97518adf6d836779149b954b9
Anjali853 <anjalianjali06149@gmail.com>
2026-03-24 23:56:21 +0530

    code

--- Median.java
+++ Median.java
@@ -1,26 +1,44 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Median {
     public static void main(String[] args) {
-    
-        Scanner sc= new Scanner(System.in);
-   
+        Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
-        int x = sc.nextInt();
-        int mid = n/2 + 1;
-        int[]arr = new int[n];
-        for(int i=0; i<n; i++){
-            arr[i]= sc.nextInt();
-        } 
-        if(mid==0){
-            System.out.println(arr[0]);
+        long k = sc.nextLong();
+
+        long[] arr = new long[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextLong();
         }
-        else if(mid==n-1){
-            System.out.println(arr[n-1]);
+
+        Arrays.sort(arr);
+
+        int mid = n / 2;
+        long ans = arr[mid];
+
+        int count = 1; 
+
+        for(int i = mid + 1; i < n; i++){
+            long diff = arr[i] - ans;
+
+            long need = diff * count;
+
+            if(k >= need){
+                k -= need;
+                ans = arr[i];
+                count++;
+            } else {
+                ans += k / count;
+                k = 0;
+                break;
+            }
         }
-        else{
-            System.out.println(arr[mid]);
+
+        if(k > 0){
+            ans += k / count;
         }
-        System.out.println();
+
+        System.out.println(ans);
     }
 }
