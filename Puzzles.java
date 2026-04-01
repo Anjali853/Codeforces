4b72b714aad2731561a23b379dc53cbb1a249f5f
Anjali853 <anjalianjali06149@gmail.com>
2026-04-01 22:23:53 +0530

    puzzles

---
+++ Puzzles.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Puzzles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();  
+        int m = sc.nextInt();  
+
+        int[] arr = new int[m];
+
+        for(int i = 0; i < m; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+
+        int minDiff = Integer.MAX_VALUE;
+
+        for(int i = 0; i <= m - n; i++){
+            int diff = arr[i + n - 1] - arr[i];
+            minDiff = Math.min(minDiff, diff);
+        }
+        System.out.println(minDiff);
+    }
+}
